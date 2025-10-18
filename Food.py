b9813b0d15d5d2c10b6085887fc2adc785626373
Harry <harryseymour.ulster@gmail.com>
2025-10-18 14:08:52 +0100

    Food spawns at a multiple of 20

--- Food.py
+++ Food.py
@@ -13,6 +13,9 @@ class Food(Turtle):
 
     # When there is a collision with the food, this method moves it to somewhere else
     def refresh(self):
-        random_x = random.randint(-280, 280)
-        random_y = random.randint(-280, 280)
+        # pick coordinates snapped to the 20px grid within safe bounds
+        possible_x = range(-280, 281, 20)
+        possible_y = range(-280, 281, 20)
+        random_x = random.choice(possible_x)
+        random_y = random.choice(possible_y)
         self.goto(random_x, random_y)
